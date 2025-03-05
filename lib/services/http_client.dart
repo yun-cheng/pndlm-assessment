1b66463c4dc50bcc8cd4b39474e0ea9addb79663
Yun-Cheng Zhang <yuncheng.dev@gmail.com>
2025-03-05 17:24:30 +0800

    feat(auth): Add Dio interceptor for accessToken injection and auto-refresh on 401 errors

--- lib/services/http_client.dart
+++ lib/services/http_client.dart
@@ -1,11 +1,40 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pndlm_assessment/constants/http.dart';
+import 'package:pndlm_assessment/repositories/auth/auth_repository.dart';
+import 'package:pndlm_assessment/storages/token_storage.dart';
 
 final dioProvider = Provider<Dio>((ref) {
   final dio = Dio(BaseOptions(baseUrl: HttpConstants.baseUrl));
 
+  final tokenStorage = ref.watch(tokenStorageProvider);
+
   dio.interceptors.add(LogInterceptor(responseBody: true));
+
+  dio.interceptors.add(
+    InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        final accessToken = await tokenStorage.getAccessToken();
+
+        if (accessToken != null) {
+          options.headers['Authorization'] = 'Bearer $accessToken';
+        }
+
+        return handler.next(options);
+      },
+      onError: (DioException e, handler) async {
+        if (e.response?.statusCode == HttpStatus.unauthorized) {
+          await ref.read(authRepositoryProvider).refreshAccessToken();
+
+          return handler.resolve(await dio.fetch(e.requestOptions));
+        }
+
+        return handler.next(e);
+      },
+    ),
+  );
 
   return dio;
 });
